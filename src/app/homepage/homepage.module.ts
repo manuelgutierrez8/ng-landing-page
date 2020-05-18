8ffae10a437964836ae758084ddeb1a3ddb153c8
Manuel <manuel.gutierrezrojas8@gmail.com>
2020-05-17 19:41:18 -0600

    Who we are section

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SectionsModule } from '../sections/sections.module';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LocationsComponent } from './locations/locations.component';
 
 
 
 @NgModule({
-  declarations: [HomepageComponent],
+  declarations: [HomepageComponent, LocationsComponent],
   imports: [
     CommonModule,
     SectionsModule
