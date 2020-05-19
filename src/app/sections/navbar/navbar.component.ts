bb33c5ec03914a9435165b5e9913a0485e3fee1a
Manuel <manuel.gutierrezrojas8@gmail.com>
2020-05-19 02:16:38 -0600

    Fixing mobile menu

--- src/app/sections/navbar/navbar.component.ts
+++ src/app/sections/navbar/navbar.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  mobileOpened = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMenuOpened() {
+    this.mobileOpened = !this.mobileOpened;
   }
 
 }
